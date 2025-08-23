ed5621c137cf17ed4da98b0708dc31e126f39520
Rye <rye@alchemyviewer.org>
2025-08-23 02:39:48 -0400

    Add support for packaging AVX libs on windows

--- build-cmd.sh
+++ build-cmd.sh
@@ -39,55 +39,65 @@ pushd "$ZLIB_SOURCE_DIR"
         windows*)
             load_vsvars
 
-            mkdir -p "build_debug"
-            pushd "build_debug"
-                opts="$(replace_switch /Zi /Z7 $LL_BUILD_DEBUG)"
-                plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
-
-                cmake -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" .. -DBUILD_SHARED_LIBS=OFF -DZLIB_COMPAT:BOOL=ON \
-                        -DCMAKE_CONFIGURATION_TYPES="Debug" \
-                        -DCMAKE_C_FLAGS_DEBUG="$plainopts" \
-                        -DCMAKE_CXX_FLAGS_DEBUG="$opts /EHsc" \
-                        -DCMAKE_MSVC_DEBUG_INFORMATION_FORMAT="Embedded" \
-                        -DCMAKE_INSTALL_PREFIX="$(cygpath -m $stage)" \
-                        -DCMAKE_INSTALL_LIBDIR="$(cygpath -m "$stage/lib/debug")" \
-                        -DCMAKE_INSTALL_INCLUDEDIR="$(cygpath -m "$stage/include/zlib-ng")"
-
-                cmake --build . --config Debug --parallel $AUTOBUILD_CPU_COUNT
-                cmake --install . --config Debug
-
-                # conditionally run unit tests
-                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                    ctest -C Debug --parallel $AUTOBUILD_CPU_COUNT
+            for arch in sse avx2 arm64 ; do
+                platform_target="x64"
+                if [[ "$arch" == "avx2" ]]; then
+                    opts="$(replace_switch /arch:SSE4.2 /arch:AVX2 $opts)"
+                elif [[ "$arch" == "avx2" ]]; then
+                    platform_target="ARM64"
                 fi
 
-                mv "$stage/lib/debug/zlibstaticd.lib" "$stage/lib/debug/zlibd.lib"
-            popd
+                mkdir -p "build_debug_$arch"
+                pushd "build_debug_$arch"
+                    opts="$(replace_switch /Zi /Z7 $LL_BUILD_DEBUG)"
+                    plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
 
-            mkdir -p "build_release"
-            pushd "build_release"
-                opts="$(replace_switch /Zi /Z7 $LL_BUILD_RELEASE)"
-                plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
+                    cmake -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" .. -DBUILD_SHARED_LIBS=OFF -DZLIB_COMPAT:BOOL=ON \
+                            -DCMAKE_GENERATOR_PLATFORM="$platform_target" \
+                            -DCMAKE_CONFIGURATION_TYPES="Debug" \
+                            -DCMAKE_C_FLAGS_DEBUG="$plainopts" \
+                            -DCMAKE_CXX_FLAGS_DEBUG="$opts /EHsc" \
+                            -DCMAKE_MSVC_DEBUG_INFORMATION_FORMAT="Embedded" \
+                            -DCMAKE_INSTALL_PREFIX="$(cygpath -m $stage)" \
+                            -DCMAKE_INSTALL_LIBDIR="$(cygpath -m "$stage/lib/$arch/debug")" \
+                            -DCMAKE_INSTALL_INCLUDEDIR="$(cygpath -m "$stage/include/zlib-ng")"
 
-                cmake -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" .. -DBUILD_SHARED_LIBS=OFF -DZLIB_COMPAT:BOOL=ON \
-                        -DCMAKE_CONFIGURATION_TYPES="Release" \
-                        -DCMAKE_C_FLAGS="$plainopts" \
-                        -DCMAKE_CXX_FLAGS="$opts /EHsc" \
-                        -DCMAKE_MSVC_DEBUG_INFORMATION_FORMAT="Embedded" \
-                        -DCMAKE_INSTALL_PREFIX="$(cygpath -m $stage)" \
-                        -DCMAKE_INSTALL_LIBDIR="$(cygpath -m "$stage/lib/release")" \
-                        -DCMAKE_INSTALL_INCLUDEDIR="$(cygpath -m "$stage/include/zlib-ng")"
+                    cmake --build . --config Debug --parallel $AUTOBUILD_CPU_COUNT
+                    cmake --install . --config Debug
 
-                cmake --build . --config Release --parallel $AUTOBUILD_CPU_COUNT
-                cmake --install . --config Release
+                    # conditionally run unit tests
+                    if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                        ctest -C Debug --parallel $AUTOBUILD_CPU_COUNT
+                    fi
 
-                # conditionally run unit tests
-                if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-                    ctest -C Release --parallel $AUTOBUILD_CPU_COUNT
-                fi
+                    mv "$stage/lib/$arch/debug/zlibstaticd.lib" "$stage/lib/$arch/debug/zlibd.lib"
+                popd
 
-                mv "$stage/lib/release/zlibstatic.lib" "$stage/lib/release/zlib.lib"
-            popd
+                mkdir -p "build_release_$arch"
+                pushd "build_release_$arch"
+                    opts="$(replace_switch /Zi /Z7 $LL_BUILD_RELEASE)"
+                    plainopts="$(remove_switch /GR $(remove_cxxstd $opts))"
+
+                    cmake -G "$AUTOBUILD_WIN_CMAKE_GEN" -A "$AUTOBUILD_WIN_VSPLATFORM" .. -DBUILD_SHARED_LIBS=OFF -DZLIB_COMPAT:BOOL=ON \
+                            -DCMAKE_CONFIGURATION_TYPES="Release" \
+                            -DCMAKE_C_FLAGS="$plainopts" \
+                            -DCMAKE_CXX_FLAGS="$opts /EHsc" \
+                            -DCMAKE_MSVC_DEBUG_INFORMATION_FORMAT="Embedded" \
+                            -DCMAKE_INSTALL_PREFIX="$(cygpath -m $stage)" \
+                            -DCMAKE_INSTALL_LIBDIR="$(cygpath -m "$stage/lib/$arch/release")" \
+                            -DCMAKE_INSTALL_INCLUDEDIR="$(cygpath -m "$stage/include/zlib-ng")"
+
+                    cmake --build . --config Release --parallel $AUTOBUILD_CPU_COUNT
+                    cmake --install . --config Release
+
+                    # conditionally run unit tests
+                    if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
+                        ctest -C Release --parallel $AUTOBUILD_CPU_COUNT
+                    fi
+
+                    mv "$stage/lib/$arch/release/zlibstatic.lib" "$stage/lib/$arch/release/zlib.lib"
+                popd
+            done
         ;;
 
         # ------------------------- darwin, darwin64 -------------------------
